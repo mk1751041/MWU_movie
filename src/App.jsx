9ada611ee2999f0d41f2f2e84dfe408da4bd7009
mk1751041 <https://github.com/mk1751041>
2023-11-14 16:25:13 +0900

    홈 하면 구현 1차

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./page/Home"
 import Movie from "./page/Movie"
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route index element={<Home />}/>
         <Route path="movie" element={<Movie />}/>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,34 @@
-import Header from "../components/Header";
+import styled from "styled-components";
+import { data } from "../data/test";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 function Home() {
     return ( 
-        <>
-            <Header />
-            <div>메인페이지내용</div>
-        </>
+        <Container>
+            <MoviePoster>
+                <Carousel
+                    showThumbs
+                    autoPlay
+                    infiniteLoop
+                    showStatus={false}
+                    transitionTime={3}
+                >
+                    {data.results.map((moive) => (
+                        <div key={moive.id}>
+                            <img src={`https://image.tmdb.org/t/p/original/${moive.backdrop_path}`} alt="" />
+                        </div>
+                    ))}
+                </Carousel>
+            </MoviePoster>
+        </Container> 
     );
 }
+
+const Container = styled.div``
+const MoviePoster = styled.div`
+    display:flex;
+    align-items: center;
+`
 
 export default Home;
