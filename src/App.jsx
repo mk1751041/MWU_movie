129040f52d609fe7573dc2d451fa86ebe1280e6d
mk1751041 <https://github.com/mk1751041>
2023-11-14 17:33:44 +0900

    서브페이지 라우팅, 페이지 작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./page/Home"
-import Movie from "./page/Movie"
+import MovieList from "./page/MovieList"
 import Header from "./components/Header"
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route index element={<Home />}/>
-        <Route path="movie" element={<Movie />}/>
+        <Route path="movies/:type" element={<MovieList />}/>
       </Routes>
     </BrowserRouter>
   )
