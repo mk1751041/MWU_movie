e715e95e246d9ac7dd25ccc670b256082977565d
mk1751041 <https://github.com/mk1751041>
2023-11-14 17:46:04 +0900

    Card 컴포넌트 작업

--- src/page/MovieList.jsx
+++ src/page/MovieList.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { config } from "../data/constant";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import Card from "../components/Card";
 
 function MovieList() {
     const [movies, setMovies] = useState([])
@@ -20,7 +21,7 @@ function MovieList() {
         <Container>
             <Title></Title>
             <Group>
-                {movies.map((movie) => <div key={movie.id}>{movie.title}</div>)}
+                {movies.map((movie) => <Card key={movie.id} movie={movie}></Card>)}
             </Group>
         </Container>
     );
